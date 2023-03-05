718dbb9832eb67b836ae8fc21a153f4b565cbe41
danielandersonBC96 <124640917+danielandersonBC96@users.noreply.github.com>
2023-03-05 01:33:51 -0400

    Update useBoneco.ts

--- src/hooks/useBoneco.ts
+++ src/hooks/useBoneco.ts
@@ -1,41 +1,58 @@
 import { useState } from "react";
 import { CharacterSides } from "../types/CharacterSide";
+import { mapSpots } from "../data/mapSpots";
 
 export const useBoneco = ( ) => {
     const [ pos, setPos ] = useState( {x: 3, y: 5} );
     const [ side, setSide] = useState< CharacterSides>( "down")
 
      const moveLeft = () => {
-
+    
         setPos( pos => ({
-             x:pos.x  -1  ,
+             x:canMove( pos.x-1,pos.y) ? pos.x -1 : pos.x ,
              y:pos.y   
         }));
+    
         setSide('left');
      }
 
      const moveRight = ( ) => {
-      setPos( pos => ({
-        x:pos.x + 1 ,
-        y:pos.y
-      }));
-      setSide('right')
+        setPos( pos => ({
+            x:canMove( pos.x+1, pos.y) ? pos.x +1 : pos.x ,
+            y:pos.y   
+       }));
+      setSide('right');
      }
 
      const moveDonw = () => {
         setPos( pos => ({
-            x:pos.x  ,
-            y:pos.y + 1 
-        }));
-        setSide("down")
+             x:pos.x,
+             y:canMove (pos.y, pos.y+1) ? pos.y + 1  : pos.y  
+       }));
+        setSide("down");
      }
 
      const moveUp = () => {
+      
         setPos( pos => ({
             x:pos.x,
-            y:pos.y -1,
+            y:canMove (pos.y, pos.y-1) ? pos.y-1 : pos.y  
         }));
+    
         setSide( 'up')
+     }
+
+     const canMove = ( x: number, y:number) => {
+        
+        if ( x < 0 || y < 0  )  return false;
+
+        if ( mapSpots[y] !== undefined && mapSpots [y][x] !== undefined){
+        if ( mapSpots[y] [x] === 1 ) {
+
+            return true;
+        }
+    }   
+        return false;
      }
 
     return{
